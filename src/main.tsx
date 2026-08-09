8142388e2955a1e9aa6c48366c4bc94c7d68ecca
Fhlo <scarletsilverfox@gmail.com>
2026-08-09 23:42:56 +0200

    Load V2 site theme styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles.css";
 import "./calendar-m3.css";
 import "./milestone3-v2.css";
+import "./theme-v2.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
