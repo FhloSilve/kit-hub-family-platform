7e704d16e05d4f09f3ef4f81693975b8cc054c66
Fhlo <scarletsilverfox@gmail.com>
2026-08-09 21:42:31 +0200

    Add Admin Worker environment bindings

--- worker/env.d.ts
+++ worker/env.d.ts
@@ -1,3 +1,7 @@
 interface Env {
   BETTER_AUTH_SECRET: string;
+  KIT_HUB_ADMIN_EMAILS?: string;
+  GITHUB_RELEASE_TOKEN?: string;
+  GITHUB_REPOSITORY?: string;
+  GITHUB_RELEASE_WORKFLOW?: string;
 }
