afa19f493c72d22be37d122a0f395dd655e56f97
Fhlo <scarletsilverfox@gmail.com>
2026-08-10 18:12:39 +0200

    Load Family Hub mobile layout fixes

--- src/main.tsx
+++ src/main.tsx
@@ -10,6 +10,7 @@ import "./dashboard-widgets.css";
 import "./dashboard-polish.css";
 import "./family-tools.css";
 import "./feedback.css";
+import "./mobile-family-hub.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
