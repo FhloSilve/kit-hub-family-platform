959461d5c33c1a9f82ae2f5434538bace8bd3a10
Fhlo <scarletsilverfox@gmail.com>
2026-08-11 12:08:40 +0200

    Load shared design system foundation

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import "./design-system.css";
 import "./styles.css";
 import "./calendar-m3.css";
 import "./milestone3-v2.css";
