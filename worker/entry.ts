d89402da5fb9b8e63614ded31996277030bfeee4
Fhlo <scarletsilverfox@gmail.com>
2026-08-11 20:30:20 +0200

    Wire beta readiness endpoints

--- worker/entry.ts
+++ worker/entry.ts
@@ -21,6 +21,7 @@ import coordinationActions from "./coordination-actions";
 import releaseState from "./release-state";
 import productOps, { markBetaTesterActive, privateBetaAccess } from "./product-ops";
 import adoptionInsights from "./adoption-insights";
+import betaReadiness from "./beta-readiness";
 import { cancelAdminRelease, dispatchAdminRelease, fetchAdminReleaseStatus, requirePlatformAdmin } from "./admin";
 import { apiError, type AppBindings } from "./http";
 
@@ -55,5 +56,5 @@ app.use("/api/v1/bootstrap", async (c, next) => {
   await next();
 });
 
-app.route("/", releaseState);app.route("/", feedback);app.route("/", familyTools);app.route("/", presence);app.route("/", productOps);app.route("/", adoptionInsights);app.route("/", coordinationActions);app.route("/", gifSearch);app.route("/", chatMedia);app.route("/", communication);app.route("/", familyHome);app.route("/", meals);app.route("/", routines);app.route("/", silviInsights);app.route("/", silviMeals);app.route("/", silviGroceries);app.route("/", silviResponder);app.route("/", silvi);app.route("/", calendarEnhancements);app.route("/", search);app.route("/", everydayV2);app.route("/", coreApp);
+app.route("/", releaseState);app.route("/", feedback);app.route("/", familyTools);app.route("/", presence);app.route("/", productOps);app.route("/", betaReadiness);app.route("/", adoptionInsights);app.route("/", coordinationActions);app.route("/", gifSearch);app.route("/", chatMedia);app.route("/", communication);app.route("/", familyHome);app.route("/", meals);app.route("/", routines);app.route("/", silviInsights);app.route("/", silviMeals);app.route("/", silviGroceries);app.route("/", silviResponder);app.route("/", silvi);app.route("/", calendarEnhancements);app.route("/", search);app.route("/", everydayV2);app.route("/", coreApp);
 export default app;
